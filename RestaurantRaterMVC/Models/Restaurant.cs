44598ab5382b9deeb4d028f74a2ca2cf1f1e7eb1
codyb1150 <codyb1150@gmail.com>
2019-04-29 10:40:38 -0400

    changes

---
+++ RestaurantRaterMVC/Models/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterMVC.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public int Name { get; set; }
+        public int Address { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class RestaurantDBContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
